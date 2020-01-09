47d3ae28137bc255df446729b995d81f6f604631
David Miller <david.miller@showpass.com>
2020-01-08 19:27:05 -0700

    0.1.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,12 +44,10 @@ module.exports = function(grunt) {
       */
     }
   });
- 
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
- 
- 
+
   grunt.registerTask('default', ['compass', 'uglify']);
 };
